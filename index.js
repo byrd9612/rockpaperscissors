18ec2b1867658c9609e40c867162cf9bd33da56b
“Kody <158676132+byrd9612@users.noreply.github.com>
2024-03-18 01:23:44 -0500

    firstpush

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const control = document.querySelector('#controls')
+const headertxt = document.querySelector('#header')
+
+
+
+function startGame() {
+    headertxt.style.display = 'none'
+}
+
+  
