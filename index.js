3f9a99383363e6b9f2c50640d3b8c49b7db82505
“Kody <158676132+byrd9612@users.noreply.github.com>
2024-03-19 06:39:30 -0500

    roughdraft

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 
-
-
-
 const playerScore = document.querySelector('#selfscore')
 const computerScore = document.querySelector('#compscore')
 const rockButton = document.querySelector('#rock')
 const paperButton = document.querySelector('#paper')
 const scissorsButton = document.querySelector('#scissors')
-const pcOptions = ["Rock", "Paper", "Scissors"]
-
-
-const playerChoice = [rockButton, paperButton, scissorsButton]
-
-
 const result = document.querySelector('#resulttext')
 
-function gamePlay() {
-    let computerChoice = pcOptions[(Math.floor(Math.random() * pcOptions.length))];
+
+let playerScoreCount = 0
+let computerScoreCount = 0
+
+function computerPlay() {
+    const pcOptions = ["Rock", "Paper", "Scissors"];
+    return pcOptions[Math.floor(Math.random() * pcOptions.length)];
+}
+
+function gamePlay(playerChoice) {
+    const computerChoice = computerPlay();
+
+    if (playerChoice === computerChoice) {
+       return result.innerHTML = `It's a tie. Computer chose ${computerChoice}.`
+    }
+  
+    else if (
+        (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Paper" && computerChoice === "Rock") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper")) 
+    {
+        result.innerHTML = `You win! Computer chose ${computerChoice}.`;
+         playerScoreCount++;
+    }
+    else if ( (playerChoice === "Rock" && computerChoice === "Paper") || (playerChoice === "Paper" && computerChoice === "Scissors") || (playerChoice === "Scissors" && computerChoice === "Rock"))
+     {
+        result.innerHTML = `You lose. Computer chose ${computerChoice}.`;
+        computerScoreCount++;
+    }
     
-    if (playerChoice[0] && computerChoice[0]) {
-       r = "It's a tie. Computer chose rock."
-    }
-    else if (playerChoice[1] && computerChoice[0]) {
-        result.innerHTML = "You win! Computer chose rock."
-    }
-    else if (playerChoice[2] && computerChoice[0]) {
-        result.innerHTML = "You lose. Computer chose rock."
-    }
-    else if (playerChoice[0] && computerChoice[1]) {
-        result.innerHTML = "You lose. Computer chose paper"
-    }
-    else if (playerChoice[1] && computerChoice[1]) {
-        result.innerHTML = "It's a tie. Computer chose paper"
-    }
-    else if (playerChoice[2] && computerChoice[1]) {
-        result.innerHTML = "You win! Computer chose paper"
-    }
-    else if (playerChoice[0] && computerChoice[2]) {
-        result.innerHTML = "You win! Computer chose scissors"
-    }
-    else if (playerChoice[1] && computerChoice[2]) {
-        result.innerHTML = "You lose. Computer chose scissors"
-    }
-    else if (playerChoice[2] && computerChoice[2]) {
-        result.innerHTML = "It's a tie. Computer chose scissors"
-    }
-};
+    updateScores();
+   
+
+}
     
 
 
+function updateScores() {
+    playerScore.textContent = playerScoreCount;
+    computerScore.textContent = computerScoreCount;
+}
 
 
+
+
+rockButton.addEventListener('click', () => gamePlay("Rock"));
+paperButton.addEventListener('click', () => gamePlay("Paper"));
+scissorsButton.addEventListener('click', () => gamePlay("Scissors"));
 
